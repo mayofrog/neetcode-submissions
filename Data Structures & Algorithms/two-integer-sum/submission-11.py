4f8acda85f64cdce7e039ad2e58895ead36af3a0
mayofrog <115328823+mayofrog@users.noreply.github.com>
2026-07-23 11:33:14 +0900

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        H = defaultdict(int)
+        for i, n in enumerate(nums):
+            a = target - n
+            if a in H: 
+                return [H[a], i]
+            H[n] = i
+        return []
