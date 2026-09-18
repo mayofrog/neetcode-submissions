cd4aeba68d2c55562b3b78a0c918263a84684004
mayofrog <115328823+mayofrog@users.noreply.github.com>
2026-09-17 23:38:15 -0700

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        H = defaultdict(int)
+        slen = len(s)
+        tlen = len(t)
+        if slen != tlen:
+            return False
+        for n in range(slen):
+            H[s[n]] += 1
+            H[t[n]] -= 1
+
+        for n in H.values():
+            if n != 0:
+                return False
+        return True
+        
